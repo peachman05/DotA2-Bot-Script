166d3d37e2df5b879c879887ec1b78ceddeeb303
DESKTOP-MRCILSS\ISAG <patcharapon1995@gmail.com>
2017-09-07 02:14:59 +0700

    fix last hit to use predict hit

--- bot_lich.lua
+++ bot_lich.lua
@@ -26,6 +26,7 @@ checkUpdate = false;
 
 maxParameter = {}
 parameter = {}
+lastTimeList = {};
 
 -- baseDamage_weight,creepDamageTaken_weight,distance_weight,hp_weight,maxReward
 -- 0.57196933267638,0.67016196996143,0.9144944026978,-0.5887051682418,29.0
@@ -111,66 +112,126 @@ function Think()
 
 			for iEnemy,creepEnemy in pairs(creepsEnemy) do
 
-				hpCreepEnemy = creepEnemy:GetHealth() 
-				baseDmageHero = npcBot:GetAttackDamage()
+				if( lastTimeList[creepEnemy] == nil)then
 
-				-- if(hpCreepEnemy  < baseDmageHero + 300)then
+					lastTimeList[creepEnemy] = {}
 
+				else
 
-					if( hpCreepEnemy  < baseDmageHero + 250 )then				
-
+					hpCreepEnemy = creepEnemy:GetHealth() 
+					baseDmageHero = npcBot:GetAttackDamage()
+					heroDamageActual = creepEnemy:GetActualIncomingDamage(baseDmageHero,DAMAGE_TYPE_PHYSICAL) 
+		
+					if( hpCreepEnemy  < baseDmageHero + 150 )then				
+					-- if( > hpCreepEnemy )then
 						
 						sumAttack = 0;
+						
+
+						timenow = GameTime()
 
 						for iAlly,creepAlly in pairs(creepsAlly) do
-							if( creepAlly:GetAttackTarget() == creepEnemy )then
+							if( creepAlly:GetAttackTarget() == creepEnemy and creepAlly:GetUnitName() == "npc_dota_creep_goodguys_ranged")then
 								sumAttack = sumAttack + creepAlly:GetAttackDamage() ; 
+								-- time = 
+								if( lastTimeList[creepEnemy][creepAlly] == nil )then
+									-- table.insert( lastTimeList, creepAlly:GetLastAttackTime() )
+									lastTimeList[creepEnemy][creepAlly] = creepAlly:GetLastAttackTime() + creepAlly:GetSecondsPerAttack() ;
+								elseif( lastTimeList[creepEnemy][creepAlly] < timenow )then
+									lastTimeList[creepEnemy][creepAlly] = nil
+								end
+								-- time = 
+
+								-- print("DotaTime: "..GameTime())
+								-- print("second: "..creepAlly:GetSecondsPerAttack())
+								-- print("attackTime: "..creepAlly:GetLastAttackTime())
+							end
+
+						end
+
+						print("----------------------------------------")
+						printTable(lastTimeList);
+
+						creepAllyMin,minValue = findMin(lastTimeList[creepEnemy]);
+						print(minValue)
+						print("time now:"..timenow)
+						if(creepAllyMin ~= nil)then
+							if(creepAllyMin:IsAlive() )then
+								creepDamageActual = creepEnemy:GetActualIncomingDamage(creepAllyMin:GetAttackDamage(),DAMAGE_TYPE_PHYSICAL)
+								if(timenow >= minValue - 1 )then
+
+									if( hpCreepEnemy-creepDamageActual <= heroDamageActual)then									
+										npcBot:Action_AttackUnit(creepEnemy,true)
+										print("----------------------attack :"..timenow)
+									end
+									print("timein ")
+									lastTimeList[creepEnemy][creepAllyMin] = nil
+								end
+
+							else
+								lastTimeList[creepEnemy][creepAllyMin] = nil
 							end
 						end
+
+						-- for iAlly,towerAlly in pairs(towersAlly) do
+						-- 	if( towerAlly:GetAttackTarget() == creepEnemy )then
+						-- 		sumAttack = sumAttack + towerAlly:GetAttackDamage() ;
+						-- 		table.insert(lastTimeList, towerAlly:GetLastAttackTime() ) 
+						-- 		table.insert(creepsList, towerAlly )
+						-- 	end
+						-- end
 
 						for iAlly,towerAlly in pairs(towersAlly) do
 							if( towerAlly:GetAttackTarget() == creepEnemy )then
 								sumAttack = sumAttack + towerAlly:GetAttackDamage() ; 
+								
+								if( lastTimeList[creepEnemy][towerAlly] == nil )then
+									
+									lastTimeList[creepEnemy][towerAlly] = towerAlly:GetLastAttackTime() + towerAlly:GetSecondsPerAttack() ;
+								elseif( lastTimeList[creepEnemy][towerAlly] < timenow )then
+									lastTimeList[creepEnemy][towerAlly] = nil
+								end
+			
 							end
+
 						end
 
-						print("sumAttack: "..sumAttack)
+						-- npcBot:Action_AttackUnit(creepEnemy,true)
+						-- print("attack!")
 
-						action = nil
-						hpNormalize =  1 - normalizeValue(hpCreepEnemy,1,250) 
-						basedamageNormalize = normalizeValue( baseDmageHero,47,125 )
-						damageTakenDesire = normalizeValue(sumAttack,0,200)
+						-- action = nil
+						-- hpNormalize =  1 - normalizeValue(hpCreepEnemy,1,250) 
+						-- basedamageNormalize = normalizeValue( baseDmageHero,47,125 )
+						-- damageTakenDesire = normalizeValue(sumAttack,0,200)
 
-						hpDesire = parameter['hp_weight'] * hpNormalize;
-						-- distanceDesire = parameter['distance_weight'] * normalizeValue( GetUnitToUnitDistance(creepEnemy,npcBot),0,900 ) ;
-						baseDamageDesire = parameter['baseDamage_weight'] * basedamageNormalize;
-						-- attackSpeedDesire = parameter['attackSpeed_weight'] * normalizeValue( npcBot:GetSecondsPerAttack(), );
-						damageTakenDesire = parameter['creepDamageTaken_weight'] * damageTakenDesire;
+						-- hpDesire = parameter['hp_weight'] * hpNormalize;						
+						-- baseDamageDesire = parameter['baseDamage_weight'] * basedamageNormalize;						
+						-- damageTakenDesire = parameter['creepDamageTaken_weight'] * damageTakenDesire;
 
-						valueDesire = hpDesire + baseDamageDesire  + damageTakenDesire;
+						-- valueDesire = hpDesire + baseDamageDesire  + damageTakenDesire;
 
-						
-						print("hpDesire : "..hpNormalize.." "..hpDesire)
-						print("basedamage :"..basedamageNormalize.." "..baseDamageDesire)
-						print("sumAttack : "..damageTakenDesire.." "..damageTakenDesire)
-						print("-------------------------------------valueDesire :"..valueDesire)
+						-- print("sumAttack: "..sumAttack)
+						-- print("hpDesire : "..hpNormalize.." "..hpDesire)
+						-- print("basedamage :"..basedamageNormalize.." "..baseDamageDesire)
+						-- print("sumAttack : "..damageTakenDesire.." "..damageTakenDesire)
+						-- print("-------------------------------------valueDesire :"..valueDesire)
 						
 						
 						-- sigmoidValue = sigmoid(valueDesire);
 
 						-- print("Value :"..valueDesire.."sigmoid :"..sigmoidValue)
 
-						if(valueDesire < 1 )then -- stop
+						-- if(valueDesire < 1 )then -- stop
 
-							-- npcBot:Action_ClearActions(true);
-							-- print("stop attack")
+						-- 	-- npcBot:Action_ClearActions(true);
+						-- 	-- print("stop attack")
 
-						else  -- attack
+						-- else  -- attack
 
-							npcBot:Action_AttackUnit(creepEnemy,true)
-							-- print("attack!")
+						-- 	npcBot:Action_AttackUnit(creepEnemy,true)
+						-- 	-- print("attack!")
 
-						end
+						-- end
 					else
 						-- if( GetUnitToUnitDistance(creepEnemy,npcBot) > npcBot:GetAttackRange() )then
 						-- 	print("Move to")
@@ -179,7 +240,7 @@ function Think()
 						
 
 					end		
-				-- end		
+				end		
 
 			end
 		end
@@ -285,3 +346,37 @@ function normalizeValue(x,min,max)
 	return (x - min)/(max - min)
 end
 
+function findMin(table)
+	min = 9999999;
+	posi = nil;
+	for key,value in pairs(table) do
+
+		if(value < min)then
+			min = value;
+			posi = key
+		end
+
+	end
+
+	return posi,min
+
+
+end
+
+function printTable(table)
+	i = 0
+	for key,value in pairs(table) do
+
+		if( key:IsNull() == false )then
+			print(i..""..key:GetUnitName())			
+			j=0;
+			for key2,value2 in pairs(value) do
+				print("+++++"..j..""..key2:GetUnitName().." "..value2)
+				j = j +1;
+			end
+			i = i +1
+		end
+
+	end
+
+end
